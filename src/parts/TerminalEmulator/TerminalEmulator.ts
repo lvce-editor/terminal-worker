33f2e7c026f5eea0f6524d8f6715d48339fa4f34
Le Vivilet <levivlet@pm.me>
2024-10-06 08:43:42 +0200

    build

--- src/parts/TerminalEmulator/TerminalEmulator.ts
+++ src/parts/TerminalEmulator/TerminalEmulator.ts
@@ -3,8 +3,9 @@ import * as Assert from '../Assert/Assert.ts'
 export const create = async ({ offscreenCanvasCursor, offscreenCanvasText, focusTextArea, handleInput }) => {
   Assert.object(offscreenCanvasCursor)
   Assert.object(offscreenCanvasText)
+  const url = '/static/js/termterm.js'
   // @ts-ignore
-  const { createOffscreenTerminal } = await import('/static/js/termterm.js')
+  const { createOffscreenTerminal } = await import(url)
   const terminal = createOffscreenTerminal({
     canvasCursor: offscreenCanvasCursor,
     canvasText: offscreenCanvasText,
