e41838385a399c953196ae210dec92e756d33c8a
Le Vivilet <levivlet@pm.me>
2024-10-06 08:50:45 +0200

    feature: use terminal dependency

--- src/parts/TerminalEmulator/TerminalEmulator.ts
+++ src/parts/TerminalEmulator/TerminalEmulator.ts
@@ -1,11 +1,9 @@
 import * as Assert from '../Assert/Assert.ts'
+import { createOffscreenTerminal } from 'termterm'
 
 export const create = async ({ offscreenCanvasCursor, offscreenCanvasText, focusTextArea, handleInput }) => {
   Assert.object(offscreenCanvasCursor)
   Assert.object(offscreenCanvasText)
-  const url = '/static/js/termterm.js'
-  // @ts-ignore
-  const { createOffscreenTerminal } = await import(url)
   const terminal = createOffscreenTerminal({
     canvasCursor: offscreenCanvasCursor,
     canvasText: offscreenCanvasText,
