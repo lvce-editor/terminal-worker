58f5e9d63b99cdb9372872ccbc5a0e0a7f65abf6
Le Vivilet <levivlet@pm.me>
2025-02-08 20:36:38 +0100

    lint

--- src/parts/IpcChild/IpcChild.ts
+++ src/parts/IpcChild/IpcChild.ts
@@ -2,12 +2,14 @@ import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
 
 export const listen = async ({ method }) => {
   const module = await IpcChildModule.getModule(method)
+  // @ts-ignore
   const rawIpc = await module.listen()
   // @ts-ignore
   if (module.signal) {
     // @ts-ignore
     module.signal(rawIpc)
   }
+  // @ts-ignore
   const ipc = module.wrap(rawIpc)
   return ipc
 }
