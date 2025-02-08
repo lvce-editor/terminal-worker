4194f14646a206142916d04b8efac23af62f59d7
Le Vivilet <levivlet@pm.me>
2025-02-08 21:12:54 +0100

    lint

--- packages/terminal-worker/src/parts/IpcParent/IpcParent.ts
+++ packages/terminal-worker/src/parts/IpcParent/IpcParent.ts
@@ -1,7 +1,7 @@
 import * as IpcParentModule from '../IpcParentModule/IpcParentModule.ts'
 
 export const create = async ({ method, ...options }) => {
-  const module = await IpcParentModule.getModule(method)
+  const module = IpcParentModule.getModule(method)
   // @ts-ignore
   const rawIpc = await module.create(options)
   if (options.raw) {
