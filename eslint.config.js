7b4a153a7dcdbd383d832c47010462bf31fe5ccd
Le Vivilet <levivlet@pm.me>
2025-02-08 20:55:06 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,12 @@
 import * as config from '@lvce-editor/eslint-config'
 
-export default [...config.default]
+export default [
+  ...config.default,
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+    },
+  },
+]
