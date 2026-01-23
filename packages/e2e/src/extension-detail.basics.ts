0392b3e7fb2c1dbf22768d415313d436772655be
Le Vivilet <levivilet@pm.me>
2026-01-23 19:20:54 +0100

    feature: simplify e2e tests

--- packages/e2e/src/extension-detail.basics.ts
+++ packages/e2e/src/extension-detail.basics.ts
@@ -1,20 +1,8 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const test: Test = async ({ Main, Locator, expect }) => {
+export const test: Test = async ({ Main }) => {
   // arrange
 
   // act
   await Main.openUri('extension-detail://builtin.theme-ayu')
-
-  // assert
-  const detailView = Locator('.ExtensionDetail')
-  await expect(detailView).toBeVisible()
-  const icon = Locator('.ExtensionDetailIcon')
-  await expect(icon).toBeVisible()
-  const name = Locator('.ExtensionDetailName')
-  await expect(name).toBeVisible()
-  const description = Locator('.ExtensionDetailDescription')
-  await expect(description).toBeVisible()
-  const tabs = Locator('.ExtensionDetailTabs')
-  await expect(tabs).toBeVisible()
 }
