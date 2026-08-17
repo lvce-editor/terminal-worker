4c2c37581e845f1db708282881092f5ccc1dcd83
Le Vivilet <levivilet@pm.me>
2026-08-17 06:07:40 -0700

    Update server dependency (#64)
    * feature: update server dependency
    
    * fix: resolve shared process from package

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,13 +1,8 @@
 import { join } from 'node:path'
-import { pathToFileURL } from 'node:url'
 import { root } from './root.js'
 import { cp } from 'node:fs/promises'
 
-const sharedProcessPath = join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
-
-const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
-
-const sharedProcess = await import(sharedProcessUrl)
+const sharedProcess = await import('@lvce-editor/shared-process')
 
 process.env.PATH_PREFIX = '/terminal-worker'
 const { commitHash } = await sharedProcess.exportStatic({
